c472254d1dafbe587708185d316ccba163e1dc2f
juandy412 <jdgarciaq@ufpso.edu.co>
2026-04-15 20:13:55 -0500

    creacion del js para la app

---
+++ desarrollo-app/actividad-blootstrap/js/script.js
@@ -0,0 +1,36 @@
+//BOTONES DE CARDS PARA ABRIR LA MODAL
+let botones = document.querySelectorAll(".btn-modal");
+
+botones.forEach(boton => {
+    boton.addEventListener("click",function (){
+        let modal =new bootstrap.Modal(document.getElementById("miModal"));
+        modal.show();
+    })
+});
+
+//validacion de formularios
+
+document.getElementById("login").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    let correo = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    let mensaje = document.getElementById("mensaje");
+
+    const USER_DEFAULT = "prueba@gmail.com";
+    const PASSWORD_DEFAULT = "123456";
+
+    if(correo === "" || password === "" ){
+        mensaje.innerHTML = "todos los campos son obligatorios!";
+        mensaje.className = "text-warning";
+        
+    } else if (correo === USER_DEFAULT && password === PASSWORD_DEFAULT){
+        mensaje.innerHTML = "Bienvenido al sistema";
+        mensaje.className = "text-success";
+
+    } else{
+        mensaje.innerHTML = "Error al procesar los datos!";
+        mensaje.className = "text-danger"
+    }
+
+})
